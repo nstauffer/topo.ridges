50f6a220e99afe41c300c795997877594595f80b
nstauffer <ngstauffer@gmail.com>
2018-07-08 09:29:49 -0700

    Clarify warning

--- R/ridgeline_topography.R
+++ R/ridgeline_topography.R
@@ -61,7 +61,7 @@ elev.density.df <- function(raster = NULL,
   # Read in the extent frame, if there is one
   if (is.null(frame)) {
     if (is.null(frame.path)) {
-      message("No frame provided.")
+      message("No frame provided; using full raster extent.")
     } else {
       if (file.exists(frame.path)) {
         frame <- rgdal::readOGR(dsn = frame.path,
